6f0f5a860b466f3e58bdcd8f7c97fce57148ff32
modione_laptop <carloowaibel@gmail.com>
2022-07-25 14:32:02 +0200

    changed permissions to be predefined in plugin.yml

--- src/main/java/me/modione/modioneplugin/commands/GameModesCommand.kt
+++ src/main/java/me/modione/modioneplugin/commands/GameModesCommand.kt
@@ -2,19 +2,17 @@ package me.modione.modioneplugin.commands
 
 import me.modione.modioneplugin.ModionePlugin
 import me.modione.modioneplugin.utils.Utils
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.GameMode
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import javax.rmi.CORBA.Util
 
 class GameModesCommand(private val mode: GameMode) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        val player: Player = Utils.getPlayer(sender, "modione.gamemodes") ?: return false
+        val player: Player = Utils.getPlayer(sender) ?: return false
         val message = "${ModionePlugin.PREFIX}${ChatColor.AQUA}You are now in ${ChatColor.RED}${mode.name.toLowerCase()}${ChatColor.AQUA} mode!"
 
         if (args.isEmpty()) {
